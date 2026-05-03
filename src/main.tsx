86de331fa75c10af85173190889ed4838bef3b18
Khantkokohein <koheinkhantko51@gmail.com>
2026-05-04 04:04:08 +0630

    Load Taurus layout fix styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppStudio from './AppStudio.tsx';
 import './index.css';
+import './layout-fix.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
